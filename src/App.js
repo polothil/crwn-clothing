a34ce4a58cbe5bdd47c47edde549fc2981a01bbe
aravindpvk <aravindpvk@gmail.com>
2021-12-05 08:22:31 +0530

    withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
-import Homepage from './pages/Homepage/Homepage';
+
+import HomePage from './pages/Homepage/Homepage';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className='App'>
-      <Homepage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './MenuItem.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
   return (
-    <div className={`menu-item ${size}`}>
+    <div className={`menu-item ${size}`} onClick={() => console.log(history.push(`${match.url}${linkUrl}`))}>
       <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }}></div>
       <div className='content'>
         <h1 className='title'>{title.toUpperCase()}</h1>
@@ -13,4 +14,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
