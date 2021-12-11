428194e7c019c1d47532a294532e6662258b570d
aravindpvk <aravindpvk@gmail.com>
2021-12-11 14:16:52 +0530

    user.types

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
 
@@ -43,15 +43,19 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={Shop} />
-          <Route path='/signin' component={SignInHome} />
+          <Route exact path='/signin' render={() => (this.props.currentUser ? <Redirect to='/' /> : <SignInHome />)} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = (state) => {
+  return { currentUser: state.user.currentUser };
+};
+
 const mapDispatchToProps = (dispatch) => {
   return { setCurrentUser: (user) => dispatch(setCurrentUser(user)) };
 };
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -2,8 +2,10 @@
 //   return { type: 'SET_CURRENT_USER', payload: user };
 // };
 
+import { UserActionTypes } from './user.types';
+
 // same as above but without using return keyword
 export const setCurrentUser = (user) => ({
-  type: 'SET_CURRENT_USER',
+  type: UserActionTypes.SET_CURRENT_USER,
   payload: user,
 });
