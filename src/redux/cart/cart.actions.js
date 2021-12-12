800dfb27aeda2bbcb426708edff06338d21c63dc
aravindpvk <aravindpvk@gmail.com>
2021-12-12 17:07:13 +0530

    add remove on checkout

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -7,3 +7,11 @@ export const toggleCartHidden = () => ({
 export const addItem = (item) => {
   return { type: CartActionTypes.ADD_ITEM, payload: item };
 };
+
+export const removeItem = (item) => {
+  return { type: CartActionTypes.REMOVE_ITEM, payload: item };
+};
+
+export const clearItemFromCart = (item) => {
+  return { type: CartActionTypes.CLEAR_ITEM_FROM_CART, payload: item };
+};
